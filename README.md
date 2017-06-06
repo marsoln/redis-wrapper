ea23b98653fe897bc495025a2b93a793229b1934
zhaobiao <zhaobiao@kakamf.com>
2017-06-07 02:01:55 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# REDIS-WRAPPER
+
+> A wrapper for node ['redis'](https://github.com/NodeRedis/node_redis) with auto connection-closing.
+
---
+++ test.js
@@ -0,0 +1,37 @@
+const redis = require('redis')
+const initRedis = require('./src/index')
+const assert = require('assert')
+
+const redisConfig = {
+  host: 'localhost',
+  port: 6379,
+}
+
+let conn = initRedis(redis, redisConfig)
+
+describe('connection', () => {
+
+  it('should set the string data correctly', async() => {
+    let input_key = 'test key'
+    let input_value = 'test value'
+
+    conn.set(input_key, input_value)
+
+    let res = await conn.get(input_key)
+
+    assert.equal(res, input_value)
+  })
+
+  it('should also be able to set a hash value', async() => {
+    let table = 'test-table'
+    let input_key = 'test key'
+    let input_value = 'test value'
+
+    conn.hset(table, input_key, input_value)
+
+    let res = await conn.hget(table, input_key)
+
+    assert.equal(res, input_value)
+  })
+
+})
